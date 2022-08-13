4d65703db2702020372def5f80df90cfee141f7d
TitoGod <mauro.dr07@gmail.com>
2022-08-12 22:31:47 -0300

    falta corregir template listar_noticias

--- RepositorioPF/fundacion/fundacion/urls.py
+++ RepositorioPF/fundacion/fundacion/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Base, name = 'base'),
+    path('', views.Home, name = 'home'),
 
     # URLS de APPS
     path('Noticias/', include('apps.noticias.urls'))
--- RepositorioPF/fundacion/fundacion/views.py
+++ RepositorioPF/fundacion/fundacion/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def Base(request):
-    return render(request, 'base.html')
+def Home(request):
+    return render(request, 'home.html')
