ec7c2599151d80202fc86f6385c0b03fbbca4f94
TitoGod <mauro.dr07@gmail.com>
2022-08-28 20:34:28 -0300

    falta filtro por fecha en noticias

--- RepositorioPF/fundacion/apps/noticias/forms.py
+++ RepositorioPF/fundacion/apps/noticias/forms.py
@@ -3,6 +3,11 @@ from pickle import TRUE
 from queue import Empty
 from django import forms
 from .models import Categoria
+from django.contrib.admin.widgets import AdminDateWidget
+
 
 class FiltroCategoriaNoticia(forms.Form):
     categoria = forms.ModelChoiceField(queryset = Categoria.objects.all())
+
+class FiltroFechaNoticia(forms.Form):
+    fecha = forms.DateField(widget=AdminDateWidget)
--- RepositorioPF/fundacion/apps/noticias/views.py
+++ RepositorioPF/fundacion/apps/noticias/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
-from .forms import FiltroCategoriaNoticia
+from .forms import FiltroCategoriaNoticia, FiltroFechaNoticia
 from django.db.models import Q
 from .models import Noticia, Comentario
 # Create your views here.
@@ -18,16 +18,21 @@ def Listar(request):
     # Buscar las noticias en la base de datos
     # Pasarlo al template
     filtro = FiltroCategoriaNoticia()
-    
     ctx['formulario_filtro'] = filtro
+
+    filtro_fecha = FiltroFechaNoticia()
+    ctx['formulario_fecha'] = filtro_fecha
     
     todas = Noticia.objects.all() # si quiero que me traiga menos noticias --> [:3] 
     
+    filter_date = request.GET.get("fecha")
     filtro_categoria = request.GET.get("categoria")
     # filtro_fecha = request.GET.get("fecha")
     
     if filtro_categoria:
         todas = Noticia.objects.filter(categoria = filtro_categoria)
+    elif filter_date:
+        todas = Noticia.objects.filter(creado = filter_date)
     else:
         todas = Noticia.objects.all()
     ctx['notis'] = todas
